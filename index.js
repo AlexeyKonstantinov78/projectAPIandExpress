dd672808bec9eba2d72ee59f8226276be89c03c0
AlexeyKonstantinov78 <alex_kav@mail.ru>
2023-01-26 16:58:46 +0300

    router

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { userRouter } from './users/users.js';
 
 const port = 8000;
 const app = express();
@@ -9,39 +10,7 @@ app.all('/hello', (req, res, next) => {
   next();
 });
 
-// отправка json
-app.get('/hello', (req, res) => {
-  res.status(201).send({ success: true }); 
-});
-
-app.get('/hello1', (req, res) => {
-  res.status(201).json({ success: true });
-});
-
-//скачать файл
-app.get('/download', (req, res) => {
-  res.download('./text.txt', 'text.txt');
-});
-
-// перенаправление
-app.get('/hello2', (req, res) => {
-  res.redirect(301, '/hello');
-});
-
-// загаловки
-app.get('/hello3', (req, res) => {
-  res.set('Content-Type', 'text/plain; charset=utf-8');
-  res.append('Warning', 'code'); // добавляем свои заголовки
-  res.type('application/json'); // добавляем тип возвращаемых данных
-  res.send('привет hello3');
-});
-
-// cokies
-app.get('/hello4', (req, res) => {
-  res.cookie('name', 'hello4'); // передаем куки
-  res.clearCookie('token');
-  res.send('привет hello3');
-});
+app.use('/users', userRouter);
 
 // обработка не существующего ссылки
 app.get('/*', (req, res) => {  
