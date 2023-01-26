8df4643d58f77de91354711256c68575c2dbafda
AlexeyKonstantinov78 <alex_kav@mail.ru>
2023-01-26 14:00:28 +0300

    Создание обычного сервера с роутером

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 import http from 'http';
+
+const host = '127.0.0.1';
+const port = 8000;
+
+const server = http.createServer((req, res) => {
+  switch (req.method) {
+    case 'GET':
+      switch (req.url) {
+        case '/hello':
+          res.statusCode = 200;  
+          res.setHeader('Content-Type', 'text/html; charset=utf-8');
+          res.end('привет');          
+          break;      
+      }
+      break;
+    default:
+      break;
+  }
+});
+
+server.listen(port, host, () => {
+  console.log(`Сервер запущен на ${host}:${port}`);
+});
