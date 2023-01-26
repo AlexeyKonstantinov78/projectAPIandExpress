7249beb7617ee288777d10eea07600d91e072b51
AlexeyKonstantinov78 <alex_kav@mail.ru>
2023-01-26 15:38:25 +0300

    символы позволяющие обрабатывать маршрут

--- index.js
+++ index.js
@@ -4,9 +4,22 @@ import express from 'express';
 const port = 8000;
 const app = express();
 
-app.get('/hello', (req, res) => {
-  res.send('привет');
+app.all('/hello', (req, res, next) => {
+  console.log('привет all');
+  next();
 });
+
+app.get('/hel+lo', (req, res) => {
+  res.send('привет'); // после буквы l + расматривает не ограниченное количексво символов так и * 
+});
+
+app.post('/hel?lo', (req, res) => {
+  res.send('привет POST'); // пройдет как hello так helo знак вопроса значит не обязательно
+});
+
+// ? - необязательно
+// + и + любое количество символов
+// regex так же принимается
 
 app.listen(port, () => {
   console.log(`Сервер запущен на http://localhost:${port}`);
