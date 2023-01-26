af12b41796e6925b5a74293c5b7ef9693671ad68
AlexeyKonstantinov78 <alex_kav@mail.ru>
2023-01-26 16:32:01 +0300

    ответ  клиенту и заголовки

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-import http from 'http';
 import express from 'express';
 
 const port = 8000;
@@ -10,35 +9,44 @@ app.all('/hello', (req, res, next) => {
   next();
 });
 
-app.get('/hel+lo', (req, res) => {
-  res.send('привет'); // после буквы l + расматривает не ограниченное количексво символов так и * 
+// отправка json
+app.get('/hello', (req, res) => {
+  res.status(201).send({ success: true }); 
 });
 
-// обработчик  midlwere 2
-const cb = (req, res, next) => {
-  console.log('cb1');
-  next();
-};
-
-app.post('/hel?lo', cb, (req, res) => {
-  res.send('привет POST'); // пройдет как hello так helo знак вопроса значит не обязательно
+app.get('/hello1', (req, res) => {
+  res.status(201).json({ success: true });
 });
 
-// ? - необязательно
-// + и + любое количество символов
-// regex так же принимается
-
-app.get('/user/hello', cb, (req, res) => {
-  res.send('привет POST'); // пройдет как hello так helo знак вопроса значит не обязательно
+//скачать файл
+app.get('/download', (req, res) => {
+  res.download('./text.txt', 'text.txt');
 });
 
-app.route('/user')
-  .get('/hello', (req, res) => {
-    res.send('привет get'); 
-  })
-  .post('/hello', (req, res) => {
-    res.send('привет post'); 
-  });
+// перенаправление
+app.get('/hello2', (req, res) => {
+  res.redirect(301, '/hello');
+});
+
+// загаловки
+app.get('/hello3', (req, res) => {
+  res.set('Content-Type', 'text/plain; charset=utf-8');
+  res.append('Warning', 'code'); // добавляем свои заголовки
+  res.type('application/json'); // добавляем тип возвращаемых данных
+  res.send('привет hello3');
+});
+
+// cokies
+app.get('/hello4', (req, res) => {
+  res.cookie('name', 'hello4'); // передаем куки
+  res.clearCookie('token');
+  res.send('привет hello3');
+});
+
+// обработка не существующего ссылки
+app.get('/*', (req, res) => {  
+  res.status(404).end();
+});
 
 app.listen(port, () => {
   console.log(`Сервер запущен на http://localhost:${port}`);
