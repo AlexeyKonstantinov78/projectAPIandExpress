d02312b1c57d8acb66d3158d5fc725a8036a9daf
AlexeyKonstantinov78 <alex_kav@mail.ru>
2023-01-26 15:50:09 +0300

    маршрутизация

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import express from 'express';
 const port = 8000;
 const app = express();
 
+// обработчик  midlwere
 app.all('/hello', (req, res, next) => {
   console.log('привет all');
   next();
@@ -13,13 +14,31 @@ app.get('/hel+lo', (req, res) => {
   res.send('привет'); // после буквы l + расматривает не ограниченное количексво символов так и * 
 });
 
-app.post('/hel?lo', (req, res) => {
+// обработчик  midlwere 2
+const cb = (req, res, next) => {
+  console.log('cb1');
+  next();
+};
+
+app.post('/hel?lo', cb, (req, res) => {
   res.send('привет POST'); // пройдет как hello так helo знак вопроса значит не обязательно
 });
 
 // ? - необязательно
 // + и + любое количество символов
 // regex так же принимается
+
+app.get('/user/hello', cb, (req, res) => {
+  res.send('привет POST'); // пройдет как hello так helo знак вопроса значит не обязательно
+});
+
+app.route('/user')
+  .get('/hello', (req, res) => {
+    res.send('привет get'); 
+  })
+  .post('/hello', (req, res) => {
+    res.send('привет post'); 
+  });
 
 app.listen(port, () => {
   console.log(`Сервер запущен на http://localhost:${port}`);
