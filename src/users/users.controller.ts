c9cdc49f8706ca46d2bc7ff0a4e668c50a19f20a
AlexeyKonstantinov78 <alex_kav@mail.ru>
2023-01-31 16:28:11 +0300

    analiz memory devTools node

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -17,7 +17,6 @@ export class UserController extends BaseContriller implements IUserController {
 		]);
 	}
 	login(req: Request, res: Response, next: NextFunction): void {
-		console.log('ds');
 		next(new HTTPError(401, 'ошибка авторизации', 'login'));
 	}
 
