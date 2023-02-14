2b8f80bf5febd5f334fc9ae14a08d4897b1a9e8e
AlexeyKonstantinov78 <alex_kav@mail.ru>
2023-02-14 15:09:08 +0300

    update e2e

--- tests/users.e2e-spec.ts
+++ tests/users.e2e-spec.ts
@@ -17,6 +17,44 @@ describe('Users e2e', () => {
 
 		expect(res.statusCode).toBe(422);
 	});
+
+	it('Login - success', async () => {
+		const res = await request(application.app)
+			.post('/users/login')
+			.send({ email: 'a2@fff.ccd', password: 'ghdhfg56666' });
+
+		expect(res.body.jwt).not.toBeUndefined();
+	});
+
+	it('Login - error', async () => {
+		const res = await request(application.app)
+			.post('/users/login')
+			.send({ email: 'a2@fff.ccd', password: '1' });
+
+		expect(res.statusCode).toBe(401);
+	});
+
+	it('Info - success', async () => {
+		const login = await request(application.app)
+			.post('/users/login')
+			.send({ email: 'a2@fff.ccd', password: 'ghdhfg56666' });
+
+		const res = await request(application.app)
+			.get('/users/info')
+			.set('Authorization', `Bearer ${login.body.jwt}`);
+
+		expect(res.body.email).toBe('a2@fff.ccd');
+	});
+
+	it('Info - error', async () => {
+		const login = await request(application.app)
+			.post('/users/login')
+			.send({ email: 'a2@fff.ccd', password: 'ghdhfg56666' });
+
+		const res = await request(application.app).get('/users/info').set('Authorization', `Bearer 1`);
+
+		expect(res.statusCode).toBe(401);
+	});
 });
 
 afterAll(() => {
